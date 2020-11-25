1bb5daaf91aec667f271b216e414e0b3e5e2674d
liuliang0519 <969884636@qq.com>
2020-11-25 23:51:57 +0800

    在idea中第一次提交项目；演示分支

--- jvmDemo/src/test/java/ZhouYang/Demo.java
+++ jvmDemo/src/test/java/ZhouYang/Demo.java
@@ -3,7 +3,8 @@ package ZhouYang;
 public class Demo{
 	public static void main(String[] args){
 		while(true){
-			System.out.println("i love U");
+			System.out.println("i love U2222333333");
+			System.out.println("oneAllin");
 		}
 	}
 }
