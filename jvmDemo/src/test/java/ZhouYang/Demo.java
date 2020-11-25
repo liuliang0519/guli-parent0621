aae3252861ae6db93cbe4ff6c8f3b9984b7e944e
liuliang0519 <969884636@qq.com>
2020-11-25 23:56:44 +0800

    在idea中第一次提交项目；演示分支

--- jvmDemo/src/test/java/ZhouYang/Demo.java
+++ jvmDemo/src/test/java/ZhouYang/Demo.java
@@ -4,7 +4,7 @@ public class Demo{
 	public static void main(String[] args){
 		while(true){
 			System.out.println("i love U2222333333");
-			System.out.println("oneAllin");
+			System.out.println("oneAllin这是dev没改");
 		}
 	}
 }
