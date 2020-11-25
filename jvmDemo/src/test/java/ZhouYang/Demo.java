db915f1b5d5eb69ac7d8cc6bbdea16d5ab8563e7
liuliang0519 <969884636@qq.com>
2020-11-25 23:56:05 +0800

    在idea中第一次提交项目；演示分支

--- jvmDemo/src/test/java/ZhouYang/Demo.java
+++ jvmDemo/src/test/java/ZhouYang/Demo.java
@@ -4,7 +4,8 @@ public class Demo{
 	public static void main(String[] args){
 		while(true){
 			System.out.println("i love U2222333333");
-			System.out.println("oneAllin");
+			System.out.println("oneAllin演示分支冲突");
+
 		}
 	}
 }
